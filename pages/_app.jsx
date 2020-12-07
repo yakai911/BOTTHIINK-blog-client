0462357c8319bd1f5f9c7e91ada9a917d2dbc5c1
Bruce Bryant <793041663@qq.com>
2020-12-07 10:37:34 +0800

    google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,9 +3,23 @@ import Header from "../components/Header";
 import "../node_modules/react-quill/dist/quill.snow.css";
 import { Provider } from "react-redux";
 import { useStore } from "../redux/store";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Provider store={store}>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+import { GA_TRACKING_ID } from "../lib/gtag";
+
 class MyDocument extends Document {
   render() {
     return (
@@ -29,9 +31,10 @@ class MyDocument extends Document {
             rel='stylesheet'
             href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
           />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -39,7 +42,7 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+            gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
